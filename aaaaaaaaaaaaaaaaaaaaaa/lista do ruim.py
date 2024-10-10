a1afaf2ed954c10e03d622a52f80d5f1f9920e4f
odanroterif <143523077+odanroterif@users.noreply.github.com>
2024-10-10 15:56:41 -0300

    Create lista do ruim.py

---
+++ aaaaaaaaaaaaaaaaaaaaaa/lista do ruim.py
@@ -0,0 +1,24 @@
+a = input("diga ola mundo: ")
+
+print(a)
+#----------------------------------
+a = input("diga um numero: ")
+
+print("o numero informado foi",a)
+#-----------------------------------
+a = int(input("diga um numero: "))
+b = int(input("diga outro numero: "))
+
+print(f"a soma de {a} + {b} é",a+b)
+#-----------------------------------
+a = float(input("primeira nota: "))
+b = float(input("segunda nota: "))
+c = float(input("terceira nota: "))
+d = float(input("quarta nota: "))
+
+print("a sua media é",(a+b+c+d)/4)
+#-----------------------------------
+a = float(input("coloque a distância em metros: "))
+
+
+print(a,"metros em centimetros é",a/100)
