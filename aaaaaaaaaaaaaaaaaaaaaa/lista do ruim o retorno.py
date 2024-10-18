9918bdd39edfdf223d69e1666d355f170f5d4abe
odanroterif <143523077+odanroterif@users.noreply.github.com>
2024-10-18 10:35:26 -0300

    Update lista do ruim o retorno.py

--- aaaaaaaaaaaaaaaaaaaaaa/lista do ruim o retorno.py
+++ aaaaaaaaaaaaaaaaaaaaaa/lista do ruim o retorno.py
@@ -21,28 +21,6 @@ notes = []
 for c in range(2):
     c = float(input("digite a nota: "))
     notes.append(c)
-#------------------------------------
-day = int(input("diga um numero de 1 a 7: "))
-
-match day:
-    case 1:
-        print("domingo")
-    case 2:
-        print("lunes")
-    case 3:
-        print("martes")
-    case 4:
-        print("miercoles")
-    case 5:
-        print("jueves")
-    case 6:
-        print("viernes")
-    case 7:
-        print("sabado")
-    case _:
-        print("esse dia ainda não existe")
-
-
 
 
 if (notes[0]*notes[1])/2 >=6:
@@ -86,3 +64,52 @@ elif (notes[0] * notes[1]) / 2 <= 4:
 else:
 
     print("\n\n\nrecuperação")
+#-----------------------------------------------
+side = []
+
+for c in range(3):
+    c = int(input(f"diga a medida do {c+1}º lado do triângulo: "))
+    side.append(c)
+
+
+
+if side[0] * side[1] * side[2] == side[0]**3:
+    print("este triângulo é equilátero")
+elif side[0] == side[1] or side[0] == side[2] or side[1] == side[2]:
+    print("este triângulo é isóceles")
+else:
+    if side[0] > side[1] and side[0] > side[2]:
+        if side[1] + side[2] > side[0]:
+            print("este triângulo é escaleno")
+        else:
+            print("não da pra formar um triângulo com essas medidas")
+    elif side[1] > side[0] and side[1] > side[2]:
+        if side[0] + side[2] > side[1]:
+            print("este triângulo é escaleno")
+        else:
+            print("não da pra formar um triângulo com essas medidas")
+    else:
+        if side[0] + side[1] > side[2]:
+            print("este triângulo é escaleno")
+        else:
+            print("não da pra formar um triângulo com essas medidas")
+#----------------------------------------------------------------------
+day = int(input("diga um numero de 1 a 7: "))
+
+match day:
+    case 1:
+        print("domingo")
+    case 2:
+        print("lunes")
+    case 3:
+        print("martes")
+    case 4:
+        print("miercoles")
+    case 5:
+        print("jueves")
+    case 6:
+        print("viernes")
+    case 7:
+        print("sabado")
+    case _:
+        print("esse dia ainda não existe")
